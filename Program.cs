3586435ed9364c08d02eaa09b62c6e26eb985a7d
Hoodstrats <hoodstrats@gmail.com>
2022-01-01 22:50:50 -0500

    - Changed certain messages and colors - Changed file creation naming scheme - Checks last file creation date to see if app was used already

--- Program.cs
+++ Program.cs
@@ -4,29 +4,33 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Reflection;
+using System.Threading;
 
 namespace Hood.Core
 {
   class Program
   {
+    public static List<string> _filesInFolder;
+
     static void Main(string[] args)
     {
       string[] lines = {
-        "Spotlight Wallpaper Grabber - Hoodstrats" + "\n" ,
-        "You know those cool wallpapers that Windows has on the login screen? Lets Yoink them."+"\n",
-        "Press S to check if Spotlight has new Wallpapers and copy them." + "\n" ,
-        "Press Q at any time if you want to Quit." + "\n"
+        $"Spotlight Wallpaper Grabber {Assembly.GetExecutingAssembly().GetName().Version} - Hoodstrats\n",
+        "This tool will Grab and Convert the Windows login Wallpapers.\n",
+        "Press S to check if Spotlight has new Wallpapers and copy them.\n" ,
+        "Press Q at any time if you want to Quit.\n"
       };
       //original console default is 120 x 30
       Console.SetWindowSize(105, 30);
-      WriteToConsole(lines[0], ConsoleColor.Yellow, true);
-      WriteToConsole(lines[1], ConsoleColor.Yellow, true);
+      WriteToConsole(lines[0], ConsoleColor.Cyan, true);
+      WriteToConsole(lines[1], ConsoleColor.Cyan, true);
       WriteToConsole(lines[2], ConsoleColor.Green);
       WriteToConsole(lines[3], ConsoleColor.Red);
 
-      SearchFolder();
+      CheckForWindowsSpotlight();
     }
-    static void SearchFolder()
+    static void CheckForWindowsSpotlight()
     {
       var pressed = Console.ReadKey(true);
       if (pressed.Key == ConsoleKey.S)
@@ -34,7 +38,7 @@ namespace Hood.Core
         string genericApp = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         string spotLightLocation = @$"{genericApp}\Appdata\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
 
-        WriteToConsole("\n" + "Searching Windows Spotlight and looking for new wallpapers..." + "\n", ConsoleColor.Green);
+        WriteToConsole("\nSearching Windows Spotlight for new wallpapers...\n", ConsoleColor.Green);
 
         if (Directory.Exists(spotLightLocation))
         {
@@ -53,15 +57,18 @@ namespace Hood.Core
 
     private static void CopyFolder(string spotLightLocation)
     {
-      //right here make it check the amount of time since last checked for update
-      //write this into a text file 
-      WriteToConsole("Looks like you have new Wallpapers to copy." + "\n", ConsoleColor.Green);
       //the @ symbol ensures that windows recognizes the "/" symbol no matter the direction
       var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
       string dest = @$"{userFolder}\Pictures\Spotlight";
 
-      WriteToConsole(@$"This will create a new folder located at {dest}. Press C to proceed." + "\n", ConsoleColor.Red);
-      WriteToConsole(@$"Press Q to quit this application." + "\n", ConsoleColor.Red);
+      if (Directory.Exists(dest))
+      {
+        WriteToConsole($"This will create a new folder located at {dest}. Press C to proceed.\n", ConsoleColor.Red);
+      }
+      else
+      {
+        WriteToConsole($"Looks like you've already setup a {dest} folder. Press C to proceed.\n", ConsoleColor.Red);
+      }
 
       var pressed = Console.ReadKey(true);
 
@@ -70,8 +77,23 @@ namespace Hood.Core
         //creates new folder if it doesn't already exist
         Directory.CreateDirectory(dest);
 
-        //create identifier for each file currently in the folder before doing anything
-        //CreateHashes(dest);
+        //populate current list of files and grab last index
+        var lastFile = GetLastFileIndex(dest);
+
+        //check if the last file's creation date is the same as current
+        if (GetIfUsedToday(_filesInFolder[lastFile]))
+        {
+          WriteToConsole($"It seems like you've already used the app today...\n", ConsoleColor.Red);
+          WriteToConsole($"Try again tommorow. Spotlight only updates so many times...\n", ConsoleColor.Red);
+
+          //delay the code execution so the message can come up
+          int milliseconds = 3000;
+          Thread.Sleep(milliseconds);
+
+          Environment.Exit(0);
+        }
+
+        WriteToConsole("Looks like there are new Wallpapers to copy.\n", ConsoleColor.Green);
 
         string[] files = Directory.GetFiles(spotLightLocation);
 
@@ -89,33 +111,15 @@ namespace Hood.Core
         Environment.Exit(0);
       }
     }
-    //cycle through the current pics and create hashes for each item
-    private static void CreateHashes(string dest)
-    {
-      string hashTable = "hashtable.txt";
-      string hashLoc = Path.Combine(dest, hashTable);
-      if (!File.Exists(hashLoc))
-      {
-        File.Create(hashLoc);
-      }
-      string[] files = Directory.GetFiles(dest);
-      foreach (string s in files)
-      {
-        string hash = Guid.NewGuid().ToString();
-        string id = $"{hash},\n";
-        File.WriteAllText(hashLoc, id);
-      }
-    }
-
     //loop through the files we copied over and check their dimensions, delete the ones that don't meet criteria
     private static void DeleteGarbage(string createdFolder)
     {
-      WriteToConsole("Deleting the Garbage..." + "\n", ConsoleColor.Red);
+      WriteToConsole("Deleting the Garbage...\n", ConsoleColor.Red);
 
       string[] files = Directory.GetFiles(createdFolder);
 
       List<string> filesToDelete = new List<string>();
-      
+
       //cycle through files grabbed and filter out the non 1080p
       //there's a lot of different mobile/tablet resolutions to be filtered
       foreach (string s in files)
@@ -141,17 +145,25 @@ namespace Hood.Core
     //organize the files after we've combed through and rename with date
     private static void RenameFiles(string createdFolder)
     {
-      WriteToConsole("Renaming files..." + "\n", ConsoleColor.Red);
+      WriteToConsole("Renaming files...\n", ConsoleColor.Red);
 
       string[] files = Directory.GetFiles(createdFolder);
 
-      int index = 0;
+      //think about making this equivalent to the number of files in the folder and set that as the current int before ++
+      //also pass the name of that last file down to do a string comparison to see if we continue with the operation
+      int index = GetLastFileIndex(createdFolder);
 
       foreach (string s in files)
       {
-        string newName = "Wallpaper" + index + " " + DateTime.Now.ToString("MMM-d-yyyy") + ".jpg";
+        //turn the date into a var that we can use to compare files
+        //add a '_' to string or some split indicator so we can use it to filter out files
+        //check to see if the date has already been used, if it has then abort operation
+        //always check the last file
+        var date = DateTime.Now.ToString("MMM-d-yyyy");
+        string newName = $"Wallpaper_{index}_{date}.jpg";
         string newFile = Path.Combine(createdFolder, newName);
 
+        //check original filename without renaming which should be series of random chars
         var named = Path.GetFileNameWithoutExtension(s);
 
         //if the file already has wallpaper in it that means it's already been renamed skip it
@@ -166,11 +178,41 @@ namespace Hood.Core
           index++;
         }
       }
-      WriteToConsole("\n" + "Done enjoy your new Wallpapers." + "\n", ConsoleColor.Green);
-      WriteToConsole("Press any key to Quit." + "\n", ConsoleColor.Red);
+      WriteToConsole("\nDone enjoy your new Wallpapers.\n", ConsoleColor.Green);
+      WriteToConsole("Press any key to Quit.\n", ConsoleColor.Red);
       Console.ReadKey(true);
       Environment.Exit(0);
     }
+
+    //returns the last index of JPG files in the folder
+    private static int GetLastFileIndex(string createdFolder)
+    {
+      _filesInFolder = new List<string>();
+      foreach (string s in Directory.GetFiles(createdFolder, "*.jpg"))
+      {
+        _filesInFolder.Add(s);
+      }
+
+      //grab the last entry in the list and make the current index that
+      return _filesInFolder.Count - 1;
+    }
+
+    //make a method that returns the creation date of the last file in the folder
+    //if the date matches today's date then we've already used the program so stop operation/delete files grabbed 
+    static bool GetIfUsedToday(string path)
+    {
+      //get file name raw so we can split at _
+      var named = Path.GetFileNameWithoutExtension(path);
+      var split = named.LastIndexOf("_") + 1;
+      var sub = named.Substring(split);
+      //substring split here 
+      if (sub == DateTime.Now.ToString("MMM-d-yyyy"))
+      {
+        return true;
+      }
+      return false;
+    }
+
     static void WriteToConsole(string line, ConsoleColor color, bool centered = false)
     {
       Console.ForegroundColor = color;
